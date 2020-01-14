9308c3d1291a82bc6c40d5b0912fc92451d4252e
Piotr Krzeminski <git@krzeminski.it>
2020-01-14 19:28:21 +0100

    Experiment with drawing a filled arc without the middle part

--- src/main/kotlin/it/krzeminski/rendering/Main.kt
+++ src/main/kotlin/it/krzeminski/rendering/Main.kt
@@ -3,6 +3,8 @@ package it.krzeminski.rendering
 import it.krzeminski.examples.piping.parallelLines
 import it.krzeminski.examples.piping.spiral
 import java.awt.*
+import java.awt.geom.Arc2D
+import java.awt.geom.Area
 
 object DrawShapesExample {
     @JvmStatic
@@ -20,8 +22,23 @@ object DrawShapesExample {
     internal class CustomPaintComponent : Component() {
         override fun paint(g: Graphics) {
             val g2d = g as Graphics2D
-            val piping = spiral
-            g2d.render(piping)
+//            val piping = parallelLines
+//            g2d.render(piping)
+            g2d.color = Color.BLUE
+            val arc2dOuter = Arc2D.Float(
+                200.0f, 200.0f,
+                300.0f, 300.0f,
+                20.0f, 70.0f,
+                Arc2D.PIE)
+            val arc2dInner = Arc2D.Float(
+                250.0f, 250.0f,
+                200.0f, 200.0f,
+                20.0f, 70.0f,
+                Arc2D.PIE)
+            val area = Area(arc2dOuter).apply {
+                subtract(Area(arc2dInner))
+            }
+            g2d.fill(area)
         }
     }
 }
