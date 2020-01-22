86b924b60378492a48d2d1fc4cd46de63129331e
Piotr Krzeminski <git@krzeminski.it>
2020-01-22 21:21:50 +0100

    Make spiral piping bigger

--- src/main/kotlin/it/krzeminski/examples/piping/Spiral.kt
+++ src/main/kotlin/it/krzeminski/examples/piping/Spiral.kt
@@ -6,7 +6,7 @@ val spiral = Pipe(
     initialOrientation = 0.degrees,
     initialPosition = Point(500.0f, 380.0f),
     radius = 10.0f,
-    pipeSegments = (1..46)
+    pipeSegments = (1..60)
         .map { PipeSegment.Arc(
             radius = 10.0f + 7.0f*it, angle = 90.degrees, direction = Direction.LEFT)
         })
